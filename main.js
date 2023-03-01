601525406002b14973081f1d5dceec07fed903ba
Santiago Carrasco <todosacarios@gmail.com>
2023-03-01 17:27:21 +0100

    5c

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("hola h")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('views/index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
